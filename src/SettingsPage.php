7a7519ea858704a3e2ebd570d56f15886bb9ff4e
leanderiversen <leander@poweredbycoffee.co.uk>
2021-03-09 12:08:59 +0000

    Initial version of 3.0-alpha

--- src/SettingsPage.php
+++ src/SettingsPage.php
@@ -5,7 +5,7 @@ namespace ICN\VCFJ;
 // Block direct access
 if(!defined('ABSPATH'))exit;
 
-class Page {
+class SettingsPage {
 
 	private static $instance = null;
 
@@ -16,18 +16,18 @@ class Page {
 		add_action( 'wp_enqueue_scripts', [$this, 'set_migrate_version'] );
 	}
 
-	public static function instance() {
+	public static function instance(): SettingsPage {
 		if( is_null( self::$instance ) ) {
 			self::$instance = new self();
 		}
 		return self::$instance;
 	}
 
-	public function add_admin_menu() { 
-		add_options_page( 'jQuery Version Control', 'jQuery Version Control', 'manage_options', 'version_control_for_jquery', [$this, 'render_page'] );
+	public function add_admin_menu(): void { 
+		add_options_page( __( 'jQuery Version Control', 'version-control-for-jquery' ), __( 'jQuery Version Control', 'version-control-for-jquery' ), 'manage_options', 'version_control_for_jquery', [$this, 'render_page'] );
 	}
 
-	public function register_settings() {
+	public function register_settings(): void {
 
 		register_setting( 'vcfj_settings_page', 'vcfj_settings' );
 
@@ -39,42 +39,35 @@ class Page {
 		);
 
 		add_settings_field( 
-			'vcfj_core_version', 
-			__( 'Select your desired jQuery version.', 'version-control-for-jquery' ), 
-			[$this, 'select_core_version'], 
+			'vcfj_core_version',
+			__( 'Select your desired jQuery version.', 'version-control-for-jquery' ),
+			[$this, 'select_core_version'],
 			'vcfj_settings_page',
-			'vcfj_pluginPage_section' 
+			'vcfj_pluginPage_section'
 		);
 
 		add_settings_field( 
 			'vcfj_migrate_version', 
-			__( 'Select your desired jQuery Migrate version.', 'version-control-for-jquery' ), 
-			[$this, 'select_migrate_version'], 
+			__( 'Select your desired jQuery Migrate version.', 'version-control-for-jquery' ),
+			[$this, 'select_migrate_version'],
 			'vcfj_settings_page',
-			'vcfj_pluginPage_section' 
+			'vcfj_pluginPage_section'
 		);
 
 		add_settings_field( 
 			'vcfj_migrate_disable', 
-			__( 'Disable jQuery Migrate?', 'version-control-for-jquery' ), 
-			[$this, 'migrate_disable'], 
+			__( 'Disable jQuery Migrate?', 'version-control-for-jquery' ),
+			[$this, 'migrate_disable'],
 			'vcfj_settings_page',
-			'vcfj_pluginPage_section' 
+			'vcfj_pluginPage_section'
 		);
 
 	}
 
-	public function select_core_version() { 
+	public function select_core_version(): void { 
 
 		$options = get_option( 'vcfj_settings' );
-		$options_core_version = $options['vcfj_core_version'];
-
-		// Check if the jQuery Core version has been set
-		if(!isset($options_core_version) || empty($options_core_version) ) {
-			$core_version = VCFJ_LATEST_CORE;
-		} else {
-			$core_version = $options_core_version;
-		}
+		$version = $options['vcfj_core_version'] ?? VCFJ::$latest_core;
 
 		$versions = [
 			'git-build' => '(Git Build)',
@@ -150,21 +143,14 @@ class Page {
 			'1.2' => '1.2.0',
 		];
 
-		$this->output_select('core', $core_version, $versions);
+		$this->output_select('core', $version, $versions);
 
 	}
 
-	public function select_migrate_version() {
+	public function select_migrate_version(): void {
 
 		$options = get_option( 'vcfj_settings' );
-		$options_migrate_version = $options['vcfj_migrate_version'];
-
-		// Check if the jQuery Migrate version has been set
-		if(!isset($options_migrate_version) || empty($options_migrate_version) ) {
-			$migrate_version = VCFJ_LATEST_MIGRATE;
-		} else {
-			$migrate_version = $options_migrate_version;
-		}
+		$version = $options['vcfj_migrate_version'] ?? VCFJ::$latest_migrate;
 
 		$versions = [
 			'git-build' => '(Git Build)',
@@ -185,11 +171,11 @@ class Page {
 			'1.0.0' => '1.0.0',
 		];
 
-		$this->output_select('migrate', $migrate_version, $versions);
+		$this->output_select('migrate', $version, $versions);
 
 	}
 
-	public function migrate_disable() {
+	public function migrate_disable(): void {
 		$options = get_option( 'vcfj_settings' );
 
 		$checked = '';
@@ -200,10 +186,10 @@ class Page {
 			}
 		}
 
-		printf( '<input type="checkbox" name="vcfj_settings[vcfj_migrate_disable]" value="1" %s />', $checked );
+		echo sprintf( '<input type="checkbox" name="vcfj_settings[vcfj_migrate_disable]" value="1" %s />', $checked );
 	}
 
-	private function output_select($type, $current, $values) {
+	private function output_select($type, $current, $values): void {
 
 		$select_name = '';
 
@@ -220,7 +206,7 @@ class Page {
 		echo '</select>';
 	}
 
-	private function output_option($name, $current, $version, $label) {
+	private function output_option(string $name, $current, $version, $label): void {
 
 		$setting_name = '';
 		$option_label_prefix = '';
@@ -233,14 +219,14 @@ class Page {
 			$option_label_prefix = 'Migrate';
 		}
 
-		printf( '<option value="%1$s" name="vcfj_settings[%2$s]" %3$s>jQuery %4$s %5$s</option>', $version, $setting_name, selected( $current, $version ), $option_label_prefix, $label );
+		echo sprintf( '<option value="%1$s" name="vcfj_settings[%2$s]" %3$s>jQuery %4$s %5$s</option>', $version, $setting_name, selected( $current, $version ), $option_label_prefix, $label );
 	}
 
-	public function section_callback() { 
-		echo '<p>' . __( 'Use the dropdown selectors below to select your desired version of jQuery. Please note that the plugin defaults to the latest stable version.', 'version-control-for-jquery' ) . '</p>';
+	public function section_callback(): void {
+		echo sprintf( '<p>%s</p>', __( 'Use the dropdown selectors below to select your desired version of jQuery. Please note that the plugin defaults to the latest stable version.', 'version-control-for-jquery' ) ); 
 	}
 
-	public function render_page() { ?>
+	public function render_page(): void { ?>
 		<div class="wrap">
 			<h1><?php _e( 'Version Control for jQuery', 'version-control-for-jquery' ); ?></h1>
 			<form action='options.php' method='post'>
@@ -253,7 +239,7 @@ class Page {
 		</div>
 	<?php }
 
-	public function set_core_version() {
+	public function set_core_version(): void {
 		// Check that the user is not viewing the administration panel
 		if ( !is_admin() ) {
 
@@ -262,25 +248,20 @@ class Page {
 
 			// Get options
 			$options = get_option( 'vcfj_settings' );
-
-			if( !isset($options['vcfj_core_version']) || empty($options['vcfj_core_version']) ) {
-				$vcfj_core_version = VCFJ_LATEST_CORE;
-			} else {
-				$vcfj_core_version = $options['vcfj_core_version'];
-			}
+			$version = $options['vcfj_core_version'] ?? VCFJ::$latest_core;
 
 			// Register the new and minified jQuery Core
-			if('git-build' === $options['vcfj_core_version'] ) {
+			if( 'git-build' === $version ) {
 				// Register the git build
-				wp_register_script( 'jquery', 'https://code.jquery.com/jquery-git.min.js', false, $vcfj_core_version );
+				wp_register_script( 'jquery', 'https://code.jquery.com/jquery-git.min.js', false, $version );
 			} else {
 				// Register the stable version
-				wp_register_script( 'jquery', 'https://code.jquery.com/jquery-' . $vcfj_core_version . '.min.js', false, $vcfj_core_version );
+				wp_register_script( 'jquery', 'https://code.jquery.com/jquery-' . $version . '.min.js', false, $version );
 			}
 		}
 	}
 
-	public function set_migrate_version() {
+	public function set_migrate_version(): void {
 		// Check that the user is not viewing the administration panel
 		if( !is_admin() ) {
 
@@ -289,23 +270,18 @@ class Page {
 
 			// Get options
 			$options = get_option( 'vcfj_settings' );
-
-			if( !isset($options['vcfj_migrate_version']) || empty($options['vcfj_migrate_version']) ) {
-				$vcfj_migrate_version = VCFJ_LATEST_MIGRATE;
-			} else {
-				$vcfj_migrate_version = $options['vcfj_migrate_version'];
-			}
+			$version = $options['vcfj_migrate_version'] ?? VCFJ::$latest_migrate;
 
 			// Check if jQuery Migrate has been disabled
 			if( !isset( $options['vcfj_migrate_disable'] ) || $options['vcfj_migrate_disable'] !== '1' ) {
 
 				// Enqueue the new and minified jQuery Migrate
-				if( 'git-build' === $options['vcfj_migrate_version'] ) {
+				if( 'git-build' === $version ) {
 					// Register the git build
-					wp_enqueue_script( 'jquery-migrate', 'https://code.jquery.com/jquery-migrate-git.min.js', [ 'jquery' ], $vcfj_migrate_version );
+					wp_enqueue_script( 'jquery-migrate', 'https://code.jquery.com/jquery-migrate-git.min.js', [ 'jquery' ], $version );
 				} else {
 					// Register the stable version
-					wp_enqueue_script( 'jquery-migrate', 'https://code.jquery.com/jquery-migrate-' . $vcfj_migrate_version . '.min.js', [ 'jquery' ], $vcfj_migrate_version );
+					wp_enqueue_script( 'jquery-migrate', 'https://code.jquery.com/jquery-migrate-' . $version . '.min.js', [ 'jquery' ], $version );
 				}
 
 			}
@@ -314,3 +290,5 @@ class Page {
 	}
 
 }
+
+SettingsPage::instance();
